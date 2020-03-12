bdae77b715a45767d194d235253335248ce8690c
Robson Fernandes <robfds@gmail.com>
2020-03-12 05:41:20 -0300

    Organizing code in modules

--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-pub fn silent_behave(silent: bool, total_bytes: usize, new_line: bool) {
-    if !silent {
-        if new_line {
-            eprintln!("\r{}", total_bytes);
-        } else {
-            eprint!("\r{}", total_bytes);
-        }
-    }
-}
