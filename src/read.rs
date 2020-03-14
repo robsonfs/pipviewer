3d743f6ed2f13133bff1fc170f52d53234d2c4bc
Robson Fernandes <robson.santos@gscap.com.br>
2020-03-14 18:38:46 -0300

    Separete input, statistics and outputs threads... An working in progress...

--- src/read.rs
+++ src/read.rs
@@ -1,14 +1,26 @@
 use crate::CHUNK_SIZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
+use std::sync::{Arc, Mutex};
 
-pub fn read(infile: &str) -> Result<Vec<u8>> {
+pub fn read_loop(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if !infile.is_empty() {
         Box::new(BufReader::new(File::open(infile)?))
     } else {
         Box::new(BufReader::new(io::stdin()))
     };
     let mut buffer = [0; CHUNK_SIZE];
-    let num_read = reader.read(&mut buffer)?;
-    Ok(Vec::from(&buffer[..num_read]))
+    loop {
+        let num_read = match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        // TODO: send this buffer to the stats thread
+        Vec::from(&buffer[..num_read]);
+    }
+    // TODO: Send an empty buffer to the stats thread
+    let mut quit = quit.lock().unwrap();
+    *quit = true;
+    Ok(())
 }
