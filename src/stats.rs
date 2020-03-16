e2fdbcd0a2dd6e9149679c670e2856ee39b62132
Robson Fernandes <robson.santos@gscap.com.br>
2020-03-15 21:21:49 -0300

    Writing some test to the stats module

--- src/stats.rs
+++ src/stats.rs
@@ -54,14 +54,32 @@ fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64
     let _ = stderr.flush();
 }
 
-trait TimeOutout {
+trait TimeOutput {
     fn as_time(&self) -> String;
 }
 
-impl TimeOutout for u64 {
+impl TimeOutput for u64 {
     fn as_time(&self) -> String {
         let (hours, left) = (*self / 3600, *self % 3600);
         let (minutes, seconds) = (left / 60, left % 60);
         format!("{}:{:02}:{:02}", hours, minutes, seconds)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::TimeOutput;
+
+    #[test]
+    fn as_time_format() {
+        let pairs = vec![
+            (5_u64, "0:00:05"),
+            (60_u64, "0:01:00"),
+            (154_u64, "0:02:34"),
+            (3603_u64, "1:00:03"),
+        ];
+        for (input, output) in pairs {
+            assert_eq!(input.as_time().as_str(), output);
+        }
     }
 }
